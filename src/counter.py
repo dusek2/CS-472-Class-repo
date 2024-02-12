386ce4671cc8338c2361439fb55163f957c7d9aa
dusek2 <dusejovsky@gmail.com>
2024-02-12 06:39:14 -0800

    adding code for deleting counter

--- src/counter.py
+++ src/counter.py
@@ -20,17 +20,28 @@ def create_counter(name):
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
-
+# PUT method to update the counter
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
+    app.logger.info(f"Request to update counter: {name}")
     if name not in COUNTERS:
         return {"Message": f"Counter {name} doesnt exists"}, status.HTTP_404_NOT_FOUND
     COUNTERS[name] += 1
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
-
+# GET method to read the counter
 @app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
+    app.logger.info(f"Request to read counter: {name}")
     if name not in COUNTERS:
         return {"Message": f"Counter {name} doesnt exists"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+# DELETE method to delete the counter
+@app.route('/counters/<name>', methods=['DELETE'])
+def delete_counter(name):
+    app.logger.info(f"Request to delete counter: {name}")
+    if name not in COUNTERS:
+        return {"Message": f"Counter does not exist"}, status.HTTP_404_NOT_FOUND
+    del COUNTERS[name]
+    return '', status.HTTP_204_NO_CONTENT
